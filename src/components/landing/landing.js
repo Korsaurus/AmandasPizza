ba921a8428065887d94a4911941a1d9f11a357cc
Korsaurus <grkorsos@gmail.com>
2021-10-07 11:56:36 -0600

    git pu

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -8,12 +8,12 @@ class Landing extends React.Component {
             <div id="reeee">
                 <div className="landingpage">
                     <h2 className="title">Slice of the day</h2>
-                    <p>Available Monday - Friday 11AM  to 1PM<br />$3.25 per slice<br />$4.25 combo with pop</p>
+                    <p>Available Monday - Friday 11AM  to 1PM<br />$3.25 per slice<br />$4.25 combo with pop</p><br />
                     <h2>Specials</h2>
                     <h3>Meal Special:</h3>
-                    <p>Two 12" <em>Pizzas + Mac and Cheese Wedges</em> $40.00<br />Add Lemon or Apple Tarts for $3.00</p>
+                    <p>Two 12" <em>Pizzas + Mac and Cheese Wedges</em> $40.00<br />Add Lemon or Apple Tarts for $3.00</p><br />
                     <h3>Single Special:</h3>
-                    <p>10" Two Topping pizza, 10 Wings and 2 Cans of Pop $24.00</p>
+                    <p>10" Two Topping pizza, 10 Wings and 2 Cans of Pop $24.00</p><br />
                     <h3>Burger Special:</h3>
                     <p>Two Mushroom Burgers $10.00</p>
                 </div>
